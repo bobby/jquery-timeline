8de553e845cd6141445f356f6deab24bdcab3e2a
bobby <bobby_calderwood@mac.com>
2010-11-08 22:50:18 -0500

    Initial commit.

---
+++ READEME.md
@@ -0,0 +1,10 @@
+# jQuery Timeline
+
+jQuery Timeline is several things:
+
+* A jQuery wrapper around the SIMILE Timeline API
+* A collection of enhancements and plugins for SIMILE Timeline
+  * A SharePoint to JSON converter
+  * A Band labeller
+  * More to come
+* Eventually, jQuery Timeline will re-implement various parts of the Timeline API using jQuery for DOM manipulation, event binding, and animation
---
+++ src/jquery.timeline.js
@@ -0,0 +1,42 @@
+(function( $ ) {
+  $.fn.timeline = function(){};
+
+  $.fn.timelineFilterHighlight = function(timeline, options){
+    options = options || {};
+    var filter-count = options['filter-count'] || 1;
+
+    var form = $("<form/>", {
+      class: "timeline-filter-highlight-controls"
+    });
+    
+    $("<div/>", {
+      text: "Filters",
+      class: "label"
+    }).appendTo(form);
+    var filters = $("<ul/>", {
+      class: "timeline-filter-controls"
+    }).appendTo(form);
+    $("<button/>" {
+      text: "Filter"
+    }).appendTo(form);
+
+    for (var i=0;i<options['filter-count'];i++) {
+      $("<input>" {
+        
+      }).wrap("li").appendTo(filters);
+    }
+
+    $("<div/>", {
+      text: "Highlights",
+      class: "label"
+    }).appendTo(form);
+    var highlights = $("<ul/>", {
+      class: "timeline-highlight-controls"
+    }).appendTo(form);
+    $("<button/>" {
+      text: "Highlight"
+    }).appendTo(form);
+
+    return this;
+  };
+})(jQuery);
